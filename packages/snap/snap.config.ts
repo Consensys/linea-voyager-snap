d82dda9109928417993b45f784f20ecacf29d9a3
Mario Christopher <marioac.dev@gmail.com>
2024-03-28 07:01:21 -0600

    Move SVG to its own file. (Alternate PR) (#34)
    Co-authored-by: Christian Montoya <christian.montoya@consensys.net>

--- packages/snap/snap.config.ts
+++ packages/snap/snap.config.ts
@@ -1,4 +1,4 @@
-import type { SnapConfig } from '@metamask/snaps-cli';
+import { merge, type SnapConfig } from '@metamask/snaps-cli';
 import { resolve } from 'path';
 
 const config: SnapConfig = {
@@ -10,6 +10,17 @@ const config: SnapConfig = {
   polyfills: {
     buffer: true,
   },
+  customizeWebpackConfig: (webpackConfig) =>
+    merge(webpackConfig, {
+      module: {
+        rules: [
+          {
+            test: /\.svg$/u,
+            type: 'asset/source',
+          },
+        ],
+      },
+    }),
 };
 
 export default config;
