c9aafd32c02ca9d9da583bffc04c0e864eb85eae
Alain Nicolas <alain.nicolas@consensys.net>
2025-06-04 16:32:29 +0200

    chore: Update dependencies

--- packages/snap/snap.config.ts
+++ packages/snap/snap.config.ts
@@ -2,7 +2,6 @@ import { merge, type SnapConfig } from '@metamask/snaps-cli';
 import { resolve } from 'path';
 
 const config: SnapConfig = {
-  bundler: 'webpack',
   input: resolve(__dirname, 'src/index.ts'),
   server: {
     port: 8080,
